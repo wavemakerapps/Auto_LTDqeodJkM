73f0ea42e3714688d4d2c892670fc5bd197464f2
root <root@d3a2e0704417>
2017-06-09 00:39:42 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_LTDqeodJkM",
+  "homePage" : "Main",
+  "name" : "Auto_LTDqeodJkM",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
